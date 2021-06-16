37fd574dd8934c8edcc395ac95fd51f340c39e46
David <david.h.gillen@gmail.com>
2021-06-15 23:16:31 -0600

    Partial implementation of logic

--- PluginFolder/Source/AB_FluidExtras/Private/ABSmartPacker.cpp
+++ PluginFolder/Source/AB_FluidExtras/Private/ABSmartPacker.cpp
@@ -1,5 +1,101 @@
-// ILikeBanas
-
+// Copyright David "Angry Beaver" Gillen, details listed on associated mods Readme
 
 #include "ABSmartPacker.h"
+float AABSmartPacker::lastRecipeCache = 0.0f;
+TArray< TSubclassOf<class UFGRecipe> > AABSmartPacker::recipeCache;
+TSubclassOf<UObject> AABSmartPacker::vanillaPacker = nullptr;
+
+AABSmartPacker::AABSmartPacker() {
+	if (vanillaPacker == nullptr) {
+		vanillaPacker = ConstructorHelpers::FClassFinder<UObject>(TEXT("/Game/FactoryGame/Buildable/Factory/Packager/Build_Packager.Build_Packager_C")).Class;
+	}
+
+	isLookingforRecipe = true;
+	currentFluidIn = nullptr;
+	currentSolidIn = nullptr;
+}
+
+// Factory interface
+//////////////////////////////////////////////////////
+void AABSmartPacker::Factory_Tick(float dt) {
+	if (isLookingforRecipe) {
+		FindRecipeFromInputs();
+	}
+
+	Super::Factory_Tick(dt);
+}
+
+void AABSmartPacker::Factory_ConsumeIngredients() {
+	Super::Factory_ConsumeIngredients();
+
+	// if we can't keep going, go shopping
+	isLookingforRecipe = !HasRequiredIngredients();
+}
+
+bool AABSmartPacker::IsConfigured() const {
+	// we auto configure ourselves so of course we're fine
+	return true;
+}
+
+// Custom
+//////////////////////////////////////////////////////
+void AABSmartPacker::tryUpdateRecipeCache(UWorld* world) {
+	// time limit function
+	float now = world->GetRealTimeSeconds();
+	//if (now < lastRecipeCache + 15.0f) { return; }
+	lastRecipeCache = now;
+
+	// set recipes on cache
+	AFGRecipeManager* rm = AFGRecipeManager::Get(world);
+	rm->GetAvailableRecipesForProducer(vanillaPacker, recipeCache);
+
+	UE_LOG(LogTemp, Warning, TEXT("Updating smart packer recipe cache [ %d ] Recipes found"), recipeCache.Num());
+}
+
+void AABSmartPacker::FindRecipeFromInputs() {
+	TSubclassOf<UFGItemDescriptor> foundFluid;
+	TSubclassOf<UFGItemDescriptor> foundSolid;
+
+	// fluid
+	if (mPipeInputConnections.Num() > 0) {
+		foundFluid = mPipeInputConnections[0]->GetFluidDescriptor();
+	} else {
+		foundFluid = nullptr;
+	}
+	
+	// solid
+	if (mFactoryInputConnections.Num() > 0) {
+		TArray< FInventoryItem > peekResult;
+		mFactoryInputConnections[0]->Factory_PeekOutput(peekResult, nullptr);
+		foundSolid = peekResult.Num() == 0 ? nullptr : peekResult[0].ItemClass;
+	} else {
+		foundSolid = nullptr;
+	}
+
+	// filter
+	if (foundFluid == currentFluidIn && foundSolid == currentSolidIn) { return; }
+
+	currentFluidIn = foundFluid;
+	currentSolidIn = foundSolid;
+
+	UE_LOG(LogTemp, Warning, TEXT("INPUTS: %s - %s"), *UFGItemDescriptor::GetItemName(currentFluidIn).ToString(), *UFGItemDescriptor::GetItemName(currentSolidIn).ToString());
+
+	if (currentFluidIn == nullptr && currentSolidIn == nullptr) { return; }
+
+	// recipe
+	tryUpdateRecipeCache(GetWorld());
+
+	// use the above references
+	// use the above references
+	// use the above references
+	// use the above references
+	// use the above references
+	// use the above references
+	// use the above references
+	// use the above references
+	// use the above references
+	// use the above references
+	// use the above references
+	// use the above references
+}
 
--- PluginFolder/Source/AB_FluidExtras/Public/ABSmartPacker.h
+++ PluginFolder/Source/AB_FluidExtras/Public/ABSmartPacker.h
@@ -1,17 +1,50 @@
-// ILikeBanas
+// Copyright David "Angry Beaver" Gillen, details listed on associated mods Readme
 
 #pragma once
 
 #include "CoreMinimal.h"
+
 #include "Buildables/FGBuildableManufacturer.h"
+#include "FGFactoryConnectionComponent.h"
+#include "FGPipeConnectionComponent.h"
+#include "FGRecipeManager.h"
+
 #include "ABSmartPacker.generated.h"
 
 /**
- * 
+ * Automatically attempts to find a valid recipe every tick when ingredients have been consumed & it can no longer produce
  */
 UCLASS()
 class AB_FLUIDEXTRAS_API AABSmartPacker : public AFGBuildableManufacturer
 {
 	GENERATED_BODY()
-	
+
+	AABSmartPacker();
+
+protected:
+	// Factory interface
+	//////////////////////////////////////////////////////
+	virtual void Factory_Tick(float dt) override;
+	//virtual void Factory_CollectInput_Implementation() override;
+	//virtual void Factory_PullPipeInput_Implementation(float dt) override;
+	virtual void Factory_ConsumeIngredients() override;
+	virtual bool IsConfigured() const override;
+
+	/******
+	void SetRecipe( TSubclassOf< class UFGRecipe > recipe );
+	******/
+
+	// Custom
+	//////////////////////////////////////////////////////
+	static float lastRecipeCache;
+	static TArray< TSubclassOf<class UFGRecipe> > recipeCache;
+	static TSubclassOf<UObject> vanillaPacker;
+
+	static void tryUpdateRecipeCache(UWorld* world);
+
+	bool isLookingforRecipe;
+	TSubclassOf<UFGItemDescriptor> currentFluidIn;
+	TSubclassOf<UFGItemDescriptor> currentSolidIn;
+
+	virtual void FindRecipeFromInputs();
 };
