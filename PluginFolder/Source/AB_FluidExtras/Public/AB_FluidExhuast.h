084d13260a037c876dd40a2def1e84c28f21a0e8
davidg <david.h.gillen@gmail.com>
2024-05-10 15:59:41 -0600

    Exhaust Prototyping

---
+++ PluginFolder/Source/AB_FluidExtras/Public/AB_FluidExhuast.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Buildables/FGBuildableFactory.h"
+#include "AB_FluidExhuast.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AB_FLUIDEXTRAS_API AAB_FluidExhuast : public AFGBuildableFactory
+{
+	GENERATED_BODY()
+	
+};
